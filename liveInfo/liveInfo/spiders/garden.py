19c403f79e0b92210420d03d8746065f5027bed5
Go Nakano <g.nakano555@gmail.com>
2019-11-05 19:35:53 +0900

    created scraping function, provided helperfunction to trim scraped data

--- liveInfo/liveInfo/spiders/garden.py
+++ liveInfo/liveInfo/spiders/garden.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
 import scrapy
-
+from liveInfo.items import LiveinfoItem
 
 class GardenSpider(scrapy.Spider):
     name = 'garden'
@@ -8,4 +7,15 @@ class GardenSpider(scrapy.Spider):
     start_urls = ['http://gar-den.in/']
 
     def parse(self, response):
-        pass
+        # print(len(response.xpath('//ul[@class="nav-menu"]/li[@id="menu-item-34"]').re(r'https?:\/\/?[-a-zA-Z]{1,256}\.[a-zA-Z]{1,6}\b\/\?m=\d+')))
+        for url in response.xpath('//ul[@class="nav-menu"]/li[@id="menu-item-34"]').re(r'https?:\/\/?[-a-zA-Z]{1,256}\.[a-zA-Z]{1,6}\b\/\?m=\d+'):
+            yield scrapy.Request(response.urljoin(url), self.parse_info)        
+        
+    def parse_info(self, response):
+        liveTitle = LiveinfoItem()
+        articles = response.css('section div article')
+        for article in articles:
+            liveTitle = article.css('div header.entry-header h1 a::text').extract_first()
+            strDate = article.css('div footer time::attr("datetime")').extract_first()[0:10]
+            lineUp = article.css('div.entry_headerpost p::text').extract()
+            print(lineUp)
